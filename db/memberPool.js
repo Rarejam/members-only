2fbd7c388e818e26b46d8e58af0e7332d7d6c55c
Jamal Rarejam <agarajamaldeen@gmail.com>
2025-07-01 17:04:59 -0400

    second commit

--- db/memberPool.js
+++ db/memberPool.js
@@ -1,10 +1,14 @@
 const { Pool } = require("pg");
 
 const memberPool = new Pool({
-  host: process.env.MEMBER_HOST,
-  user: process.env.MEMBER_USER,
-  password: process.env.MEMBER_PASSWORD,
-  port: process.env.MEMBER_DEFAULTPORT,
-  database: process.env.MEMBER_DATABASE,
+  // host: process.env.MEMBER_HOST,
+  // user: process.env.MEMBER_USER,
+  // password: process.env.MEMBER_PASSWORD,
+  // port: process.env.MEMBER_DEFAULTPORT,
+  // database: process.env.MEMBER_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Render requires this
+  },
 });
 module.exports = memberPool;
